55b6fb7a3cbe1a658034758387ce03d452ba6e62
Diegomaldonado28 <42366205+Diegomaldonado28@users.noreply.github.com>
2018-10-05 12:05:11 -0500

    shop
    proyecto1: Lopez Ceciliano Brett Antonio, Martínez Mendoza Aarón, Maldonado Gómez Diego

---
+++ shop/shop1/PRODUCT.swift
@@ -0,0 +1,18 @@
+//
+//  PRODUCT.swift
+//  shop1
+//
+//  Created by Usuario invitado on 4/10/18.
+//  Copyright © 2018 Usuario invitado. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Campo {
+    
+    var Name: String
+    var Price: String
+    var Desc: String
+    var Image: UIImage
+}
